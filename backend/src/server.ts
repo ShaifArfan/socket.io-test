7d1c2de03ddeaa0c192416b64a29646d6c2d0eff
Shaif Arfan <shaifarfan08@gmail.com>
2023-10-21 00:39:40 +0600

    events

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,19 @@ import { Server as IOServer } from 'socket.io';
 import allRoutes from './routes';
 import { portInUseErrorLogs } from './utils/showErrorLogs';
 
+const db = {
+  1: {
+    name: 'John',
+    age: 29,
+  },
+  2: {
+    name: 'Jane',
+    age: 22,
+  },
+};
+
+let string = 'server';
+
 // dotenv
 dotenv.config();
 
@@ -20,12 +33,51 @@ const io = new IOServer(httpServer, {
   },
 });
 
-io.on('connection', (socket) => {
+io.on('connect', (socket) => {
   console.log('io connected on ' + socket.id);
-});
 
-const count = io.engine.clientsCount;
-console.log(count);
+  io.use((socket, next) => {
+    console.log('middleware io use ' + socket.id);
+    next();
+  });
+
+  const count = io.engine.clientsCount;
+  console.log(count);
+  socket.join('room1');
+  console.log(socket.rooms);
+
+  socket.on('custom-event', (id, callback) => {
+    if (!id) return;
+    const data = db[id];
+    if (!data) return;
+    callback({
+      data,
+    });
+  });
+
+  // socket.broadcast.emit('broadcast', 'i am broadcasting');
+
+  socket.on('set-string', (data) => {
+    console.log(data);
+    string = data;
+
+    // socket.emit('get-string', data);
+  });
+
+  socket.broadcast.emit('get-string', string);
+
+  // io.sockets.on('disconnect', (socket) => {
+  //   socket.disconnect();
+  //   console.log('io disconnected');
+  //   const count = io.engine.clientsCount;
+  //   console.log(count);
+  // });
+
+  // io.on('error', (error) => {
+  //   console.log('error ' + error);
+  //   socket.disconnect();
+  // });
+});
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}||`);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,14 @@ import { socket } from './socket.ts';
 
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
+  const [data, setData] = useState({});
+  const [string, setString] = useState('');
+
+  // useEffect(() => {
+  //   return () => {
+  //     socket.off('get-string', listener);
+  //   };
+  // }, [string]);
 
   useEffect(() => {
     function onConnect() {
@@ -14,23 +22,54 @@ function App() {
       setIsConnected(false);
     }
 
-    // function onFooEvent(value) {
-    //   setFooEvents((previous) => [...previous, value]);
-    // }
+    function onGetString(data) {
+      console.log(data);
+      setString(data);
+    }
 
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
+    socket.on('get-string', onGetString);
     // socket.on('foo', onFooEvent);
 
     return () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
+      socket.off('get-string', onGetString);
       // socket.off('foo', onFooEvent);
     };
   }, []);
   return (
     <>
       <h1>Socket Test</h1>
+      <p>{socket.id}</p>
+      {data && <p>Data: {JSON.stringify(data)}</p>}
+      <p>{string}</p>
+      <input
+        type="text"
+        value={string}
+        onChange={(v) => {
+          setString(v.currentTarget.value);
+        }}
+      />
+      <br />
+      <button
+        onClick={() => {
+          socket.emit('set-string', string);
+        }}
+      >
+        pass
+      </button>
+      {/* <button
+        onClick={() => {
+          socket.on('get-string', (data) => {
+            console.log(data);
+            setData(data);
+          });
+        }}
+      >
+        Get
+      </button> */}
       <p>Status: {isConnected ? 'connected' : 'disconnected'}</p>
       <button
         onClick={() => {
@@ -42,7 +81,19 @@ function App() {
       </button>
       <button
         onClick={() => {
+          console.log(socket.id);
+          socket.emit('custom-event', 2, (res) => {
+            console.log(res);
+            setData(res);
+          });
+        }}
+      >
+        custom event
+      </button>
+      <button
+        onClick={() => {
           socket.disconnect();
+          // socket.emit('disconnect');
         }}
       >
         Disconnect IO
